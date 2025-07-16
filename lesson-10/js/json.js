21631fc71a556996a43b6b952c08e9fdc5151dbd
Sarah Perdomo <sarahdanielap@gmail.com>
2025-07-16 18:53:42 -0400

    week 10

--- lesson-10/js/json.js
+++ lesson-10/js/json.js
@@ -1,8 +1,11 @@
 /* STEP 2: Reference the HEADER and the SECTION elements with variables */
-
+const header = document.querySelector("header");
+const section = document.querySelector("section");
 
 // STEP 3a: Create the asynchronous function populate()
-
+async function populate() {
+    
+}
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
     
